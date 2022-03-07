0b01c86c732d8bf411c96500c57d4a75b7ac9c51
navid imani <52500755+navidimani-sisco@users.noreply.github.com>
2022-03-07 18:31:05 +0330

    update basic view

---
+++ view/add_user.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ثبت نام</title>
+</head>
+<body>
+    <h1>ثبت نام</h1>
+    <form action="" method="post">
+        <div>
+            <label for="fullname">نام کامل</label>
+            <input type="text" name="fullname" id="fullname">
+        </div>
+        <div>
+            <label for="username">نام کاربری</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div>
+            <label for="password">کلمه عبور</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <button type="submit" name = "submit">ثبت نام</button>
+    </form>
+</body>
+</html>
